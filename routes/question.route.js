8215a6c7879935980c4abe2ea7be2aa4ea4c3cda
Reegansuseethar <32837142+Reegansuseethar@users.noreply.github.com>
2020-08-24 15:59:39 +0530

    Update question.route.js

--- routes/question.route.js
+++ routes/question.route.js
@@ -5,7 +5,7 @@ const questionRoute = express.Router();
 let Question = require('../models/question');
 
 // Add Question
-questionRoute.route('/add-question').post((req, res, next) => {
+questionRoute.route('/addQuestion').post((req, res, next) => {
     Question.create(req.body, (error, data) => {
         if (error) {
             return next(error)
@@ -26,35 +26,32 @@ questionRoute.route('/getAll').get((req, res) => {
     })
 })
 
-// // Get single Question
-// questionRoute.route('/read-question/:id').get((req, res) => {
-//   Question.findById(req.params.id, (error, data) => {
-//     if (error) {
-//       return next(error)
-//     } else {
-//       res.json(data)
-//     }
-//   })
-// })
+// Get single Question
+questionRoute.route('/getQuestionById/:id').get((req, res) => {
+    Question.findById(req.params.id, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+})
 
-
-// // Update Question
-// questionRoute.route('/update-question/:id').put((req, res, next) => {
-//   Question.findByIdAndUpdate(req.params.id, {
-//     $set: req.body
-//   }, (error, data) => {
-//     if (error) {
-//       return next(error);
-//       console.log(error)
-//     } else {
-//       res.json(data)
-//       console.log('Question successfully updated!')
-//     }
-//   })
-// })
+// Update Question
+questionRoute.route('/updateQuestion/:id').put((req, res, next) => {
+    Question.findByIdAndUpdate(req.params.id, {
+        $set: req.body
+    }, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.status(200).json({ message: 'Question updated successfully.!' });
+        }
+    })
+})
 
 // Delete Question
-questionRoute.route('/delete-question/:id').delete((req, res, next) => {
+questionRoute.route('/deleteQuestion/:id').delete((req, res, next) => {
     Question.findByIdAndRemove(req.params.id, (error, data) => {
         if (error) {
             return next(error);

de26090a7f6924ae3e6221394723860b5dfa3b44
Reegansuseethar <32837142+Reegansuseethar@users.noreply.github.com>
2020-09-20 19:39:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,10 +25,8 @@ const groupRoute = require('./routes/group.route')
 const subgroupRoute = require('./routes/subgroup.route')
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.json({limit:'50mb'}));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 app.use(cors());
 
 // RESTful API root
